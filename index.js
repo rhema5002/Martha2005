0e1fb975c6e731631bd4810127cf975158a3e0d9
rhema5002 <141932006+rhema5002@users.noreply.github.com>
2023-09-22 12:44:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✅ STARTING...')
+hiconsole.log('✅ STARTING...')
 
 import { join, dirname } from 'path'
 import { createRequire } from 'module';
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('Martha2005', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @Martha2005._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
